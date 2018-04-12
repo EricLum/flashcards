17ad4bb5927b1c909d8ba093dc2034d4b13725e1
eal6gf <elum1234@gmail.com>
2018-04-12 18:26:14 -0400

    add basic enzyme/jest tests

---
+++ src/__test__/Card.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Card from '../containers/Card'
+
+describe('Card', () => {
+  it('should be defined', () => {
+    expect(Card).toBeDefined();
+  });
+})
