034e40d00936b772ad863aec8b3e563f7ca4eb9e
eal6gf <elum1234@gmail.com>
2018-04-14 15:42:09 -0400

    more work on form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './containers/Card'
 import Deck from './containers/Deck'
+import Form from './containers/Form'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Card></Card>
         <Deck title='hey man'></Deck>
+        <Form></Form>
       </div>
     );
   }
--- src/containers/Form.js
+++ src/containers/Form.js
@@ -3,28 +3,39 @@ import React, {Component} from 'react'
 class Form extends Component {
   constructor(props){
     super(props)
-    this.handleInput = handleInput.bind(this)
+    this.handleInput = this.handleInput.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   state = {
-    question: ''
-    answer: ''
+    question: '',
+    answer: '',
     deckId: 0
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    console.log('submitted')
   }
 
   handleInput(e){
     this.setState({
-      question: e.target.value
-    })
+      [e.target.name]: e.target.value
+    }, console.log(this.state))
+  }
+  onSubmit(e){}
 
   render(){
     return(
-      <form></form>
+      <form onSubmit={this.handleSubmit} onChange={this.handleInput}>
+        <label>Question: </label>
+        <input type='text' name='question'/>
+        <label>Answer: </label>
+        <input type='text' name='answer' />
+        <input type='submit' />
+      </form>
     )
   }
-  }
-
-
 }
 
 export default Form
